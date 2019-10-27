2d66b34dac8c2b52b7d2766d66b62e38915866cc
Zoran Obradović <zockyo@gmail.com>
2019-10-27 21:06:49 +0100

    patch

--- web-shell-app.mjs
+++ web-shell-app.mjs
@@ -1,3 +1,25 @@
 import { RemoteSlavePort } from "@websh/remote-slave-port";
 export const WebShellApp = new RemoteSlavePort('SOUTH-TOOTH');
+
+/**
+ * Apps can override these
+ */
+
 WebShellApp.command('proc-close',()=>true);
+WebShellApp.command('proc-init',()=>true);
+
+/**
+ * Autofocus - so we can switch between windows and keep the correct focus
+ * TODO: Add opt-out option for this, which means that it should run when the
+ * remote slave port is first connected.
+ */
+window.addEventListener("load", ()=> {
+  document.body.addEventListener('focus', e=>{
+    WebShellApp.lastActiveElement = e.target;
+  },true)
+  window.addEventListener('focus', e=>{
+    if (WebShellApp.lastActiveElement && document.activeElement === document.body) {
+      requestIdleCallback(()=>WebShellApp.lastActiveElement.focus())
+    }
+  }) 
+})
